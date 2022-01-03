29e8ccfcddab0b25bc3a99c48a2cb42e955a5848
tanmayVaish <tanmay777leon@gmail.com>
2022-01-04 02:03:58 +0530

    frontend created

--- src/App.js
+++ src/App.js
@@ -2,14 +2,14 @@ import './App.css';
 
 function App() {
   return (
-    <div className={'app'}>
-      <div className={'container'}>
-        <div className={'startTitle text-9xl'}>Does Size Matter?</div>
+    <div className="app">
+      <div className={'w-fit flex items-end'}>
+        <div className={'startTitle p-0 text-8xl text-white'}>Does Size Matter?</div>
         <form action={''} method={'POST'} className={'input w-full max-w-sm'}>
           <div className={'flex items-center border-b border-white py-3'}>
             <input
               className={
-                'appearance-none bg-transparent border-none w-full text-white mr-3 py-1 px-2 leading-tight focus:outline-none'
+                'appearance-none bg-transparent border-none w-full text-white mr-3 py-1 px-2 leading-tight focus:outline-none text-1xl'
               }
               required={true}
               type={'text'}
@@ -34,41 +34,31 @@ function App() {
             </button>
           </div>
         </form>
-        <div className={'endTitle'}>and Find Out!</div>
+        <div className={'endTitle text-white text-8xl'}>and Find Out!</div>
       </div>
-
       <div className="tableContainer">
-        <table class="min-w-full divide-y divide-gray-200">
-          <thead className={'bg-gray-50'}>
+        <table class="min-w-full divide-y divide-white mt-4">
+          <thead className={'bg-gray-500'}>
             <tr>
               <th
                 scope={'col'}
                 className={
-                  'px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider'
+                  'px-9 py-3 text-left text-xs font-medium text-white uppercase tracking-wider'
                 }
               >
-                Name
+                Long-URL
               </th>
               <th
                 scope="col"
-                className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
+                className="px-6 py-3 text-left text-xs font-medium text-white uppercase tracking-wider"
               >
-                Title
+                Short-URL
               </th>
               <th
                 scope="col"
-                className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
+                className="px-6 py-3 text-left text-xs font-medium text-white uppercase tracking-wider"
               >
-                Status
-              </th>
-              <th
-                scope="col"
-                className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
-              >
-                Role
-              </th>
-              <th scope="col" className="relative px-6 py-3">
-                <span className="sr-only">Edit</span>
+                Clicks
               </th>
             </tr>
           </thead>
@@ -76,109 +66,26 @@ function App() {
             <tr>
               <td className="px-6 py-4 whitespace-nowrap">
                 <div className="flex items-center">
-                  <div className="flex-shrink-0 h-10 w-10">
-                    <img
-                      className="h-10 w-10 rounded-full"
-                      src="https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=4&w=256&h=256&q=60"
-                      alt=""
-                    />
-                  </div>
                   <div className="ml-4">
                     <div className="text-sm font-medium text-gray-900">
-                      Jane Cooper
-                    </div>
-                    <div className="text-sm text-gray-500">
-                      jane.cooper@example.com
+                      https://github.com/tanmayVaish/url-shortner
                     </div>
                   </div>
                 </div>
               </td>
               <td className="px-6 py-4 whitespace-nowrap">
                 <div className="text-sm text-gray-900">
-                  Regional Paradigm Technician
+                  urlshorten.xyz
                 </div>
-                <div className="text-sm text-gray-500">Optimization</div>
               </td>
               <td className="px-6 py-4 whitespace-nowrap">
                 <span className="px-2 inline-flex text-xs leading-5 font-semibold rounded-full bg-green-100 text-green-800">
-                  Active
+                  2
                 </span>
-              </td>
-              <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-500">
-                Admin
-              </td>
-              <td className="px-6 py-4 whitespace-nowrap text-right text-sm font-medium">
-                <a href="#" className="text-indigo-600 hover:text-indigo-900">
-                  Edit
-                </a>
               </td>
             </tr>
           </tbody>
-        </table><table className="min-w-full divide-y divide-gray-200">
-              <thead className="bg-gray-50">
-                <tr>
-                  <th
-                    scope="col"
-                    className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
-                  >
-                    Name
-                  </th>
-                  <th
-                    scope="col"
-                    className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
-                  >
-                    Title
-                  </th>
-                  <th
-                    scope="col"
-                    className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
-                  >
-                    Status
-                  </th>
-                  <th
-                    scope="col"
-                    className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
-                  >
-                    Role
-                  </th>
-                  <th scope="col" className="relative px-6 py-3">
-                    <span className="sr-only">Edit</span>
-                  </th>
-                </tr>
-              </thead>
-              <tbody className="bg-white divide-y divide-gray-200">
-                {people.map((person) => (
-                  <tr key={person.email}>
-                    <td className="px-6 py-4 whitespace-nowrap">
-                      <div className="flex items-center">
-                        <div className="flex-shrink-0 h-10 w-10">
-                          <img className="h-10 w-10 rounded-full" src={person.image} alt="" />
-                        </div>
-                        <div className="ml-4">
-                          <div className="text-sm font-medium text-gray-900">{person.name}</div>
-                          <div className="text-sm text-gray-500">{person.email}</div>
-                        </div>
-                      </div>
-                    </td>
-                    <td className="px-6 py-4 whitespace-nowrap">
-                      <div className="text-sm text-gray-900">{person.title}</div>
-                      <div className="text-sm text-gray-500">{person.department}</div>
-                    </td>
-                    <td className="px-6 py-4 whitespace-nowrap">
-                      <span className="px-2 inline-flex text-xs leading-5 font-semibold rounded-full bg-green-100 text-green-800">
-                        Active
-                      </span>
-                    </td>
-                    <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-500">{person.role}</td>
-                    <td className="px-6 py-4 whitespace-nowrap text-right text-sm font-medium">
-                      <a href="#" className="text-indigo-600 hover:text-indigo-900">
-                        Edit
-                      </a>
-                    </td>
-                  </tr>
-                ))}
-              </tbody>
-            </table>
+        </table>
       </div>
     </div>
   );
