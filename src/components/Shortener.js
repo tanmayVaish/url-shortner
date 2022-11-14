ed4ffee9724135138084b1cd3c37ce00dc56acf4
tanmayVaish <tanmay777leon@gmail.com>
2022-11-14 23:24:31 +0530

    minor change

--- src/components/Shortener.js
+++ src/components/Shortener.js
@@ -74,7 +74,7 @@ const Shortener = () => {
               }
               required={true}
               type={'text'}
-              placeholder={'Insert Custom URL Here!'}
+              placeholder={'Insert Custom ID Here!'}
               aria-label={'Short Url'}
               onChange={(e) => {
                 setShort(e.target.value);
